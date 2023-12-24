f7840b89edbc6a050ed939fbb86fda6d27cbb22d
David Bauer <davbauer@ucdavis.edu>
2023-12-23 18:41:29 -0800

    Day 12 (incomplete)

---
+++ src/day12.rs
@@ -0,0 +1,16 @@
+mod utils;
+
+fn part1() {
+    const FILE: &str = "./inputs/day12_ex1.txt";
+    let lines = utils::read_lines_in_file(FILE);
+}
+
+fn part2() {
+    const FILE: &str = "./inputs/day12_ex1.txt";
+    let lines = utils::read_lines_in_file(FILE);
+}
+
+fn main() {
+    part1();
+    part2();
+}
